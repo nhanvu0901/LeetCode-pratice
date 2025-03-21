6994160c10b9cb1201297eba2a72b016425bbf8f
Vu Trong Nhan <vutrongnhan@luvina.net>
2025-03-21 16:42:36 +0700

    code

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -2,22 +2,58 @@ import math
 
 
 # class Solution:
-    # def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-    #     pass
-    #both are sorted array so when merge check the last and first of the array
+# def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+#     pass
+# both are sorted array so when merge check the last and first of the array
 
-def binarySearch(arr,num,last_index,first_index):
+def binarySearch(arr, num, last_index, first_index):
     median_index = math.ceil((last_index + first_index) / 2)  # 2.5 -> 3
     median = arr[median_index]
     if median == num:
         return median_index
+    if first_index >= last_index - 1:
+        if num > arr[first_index]:
+            return first_index + 1
+        else:
+            return first_index
     if num > median:
-        return binarySearch(arr,num,last_index,median_index)
+        return binarySearch(arr, num, last_index, median_index)
     else:
-        return binarySearch(arr,num,median_index,first_index)
+        return binarySearch(arr, num, median_index, first_index)
 
-if __name__ == "__main__":
-    test = [5, 12, 23, 34, 45, 56, 67, 78, 89, 91, 93, 98, 105, 112, 119, 125, 130, 137, 142, 148, 153, 159, 164, 170, 175, 181, 186, 192, 197, 200]
-    find = 78
-    result = binarySearch(test,find,len(test)-1,0)
-    print(result)
+
+def insertNumber(arr, num):
+    index = binarySearch(arr, num, 0, len(arr) - 1)
+    return arr.insert(index, num)
+
+def checkCase(first_array, second_array):
+    if first_array[-1] > second_array[-1]:
+        return second_array, first_array  # a_array, b_array
+    else:
+        return first_array, second_array
+
+def mergeArr(first_array,second_array):
+    a_array, b_array = checkCase(first_array, second_array)
+
+    if b_array[len(b_array)-1] > a_array[len(a_array)-1]:
+        if b_array[0] < a_array[0]:#TH nam trong
+            return 'nam trong'
+        elif a_array[0] <= b_array[0] <= a_array[len(a_array)-1]:
+            if a_array[0] == b_array[0]:
+                pass
+            elif  b_array[0] <= a_array[-1]:
+                pass
+            else:
+                pass
+        else:
+            return 'ngoai khoang'
+
+
+
+test_array  = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13]
+best_array = [ 13, 14, 15, 16]
+print(mergeArr(test_array,best_array))
+# input_num = 8
+# index = binarySearch(test_array, input_num, len(test_array) - 1, 0)
+# test_array.insert(index, input_num)
+# print(test_array)
